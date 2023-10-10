c6b3dd22c6a23b564dffc7c1771fce2581d46bfd
Antonio López <tonidiez17@gmail.com>
2023-10-10 19:01:49 +0200

    prueba

---
+++ src/main/java/com/example/agitana/AgitanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.agitana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgitanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgitanaApplication.class, args);
+	}
+
+}
