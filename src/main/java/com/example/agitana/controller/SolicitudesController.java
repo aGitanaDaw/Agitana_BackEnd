45b1a0530e8ca9b420bb3c435e27a98b1f65a6cd
carlitoscgm27 <cgarciamadiedo@safareyes.es>
2023-10-16 20:55:40 +0200

    subida carlos

---
+++ src/main/java/com/example/agitana/controller/SolicitudesController.java
@@ -0,0 +1,25 @@
+package com.example.agitana.controller;
+import com.example.agitana.models.Solicitudes;
+import com.example.agitana.service.SolicitudesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController()
+@RequestMapping(path = "/Solicitudes")
+public class SolicitudesController {
+    //crear, eliminar y listar
+    private final SolicitudesService solicitudesService;
+
+    @Autowired
+    public SolicitudesController(SolicitudesService solicitudesService) {
+        this.solicitudesService = solicitudesService;
+    }
+
+    // @GetMapping(value = "/listar")
+    // public List<Solicitudes> listarSolicitudes() {
+    // return solicitudesService.listarSolicitudes();
+    // }
+}
---
+++ src/main/java/com/example/agitana/models/Solicitudes.java
@@ -0,0 +1,32 @@
+package com.example.agitana.models;
+import com.example.agitana.enums.TipoSolicitud;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "solicitudes", schema ="public" , catalog = "postgres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Solicitudes {
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "estado")
+    @Enumerated(EnumType.ORDINAL)
+    private TipoSolicitud tipoSolicitud;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_almacen")
+    private com.example.agitana.models.almacen almacen;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_persona")
+    private com.example.agitana.models.persona persona;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_categoria")
+    private com.example.agitana.models.categoria categoria;
+}
